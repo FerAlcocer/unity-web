e7dccc01b689667da218404f084c928104b42b6f
Freischarler <martin.paz@live.com.ar>
2022-02-08 11:23:51 -0300

    Change cards to button home page

--- src/components/Home/HomeCards.js
+++ src/components/Home/HomeCards.js
@@ -1,62 +1,68 @@
 import React from 'react';
-import { Card, Button, CardGroup } from 'react-bootstrap';
+import {  Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
 export const HomeCards = () => {
   return (
-    <CardGroup className="animate__animated animate__fadeIn">
-        <Card   style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Kids Class</Card.Title>
-          
-          <Link to={ `./program/0` }>
-          <div className="d-grid gap-2">
-            <Button variant="primary" size="lg" >LEARN MORE</Button>
-          </div>
-          </Link>
-        </Card.Body>
-        </Card>
+    <>
 
-        <Card   style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Intro Class</Card.Title>
-          
-          <Link to={ `./program/0` }>
-          <div className="d-grid gap-2">
-            <Button variant="primary" size="lg" >LEARN MORE</Button>
-          </div>
-          </Link>
-        </Card.Body>
-        </Card>
+  <div className="row">
+    <div className="col mb-4 text-center">
+    <Link to={ `./program/1` }>
+    <Button className="card-title button-box" variant="dark">KIDS CLASS</Button>
+    </Link>
+    </div>
+    
 
-        <Card   style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Schedule</Card.Title>
-          
-          <Link to={ `./program/0` }>
-          <div className="d-grid gap-2">
-            <Button variant="primary" size="lg" >LEARN MORE</Button>
-          </div>
-          </Link>
-        </Card.Body>
-        </Card>
+    <div className="col  mb-4 text-center">
+    <Link to={ `./program/0` }>
+    <Button className="card-title button-box" variant="dark">INTRODUCTION CLASS</Button>
+    </Link>
+    </div>
 
-        <Card   style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Instructors</Card.Title>
-          
-          <Link to={ `./instructors` }>
-          <div className="d-grid gap-2">
-            <Button variant="primary" size="lg" >LEARN MORE</Button>
-          </div>
-          </Link>
-        </Card.Body>
-        </Card>
+    <div className="col  mb-4 text-center">
+    <Link to={ `./schedule` }>
+    <Button className="card-title button-box" variant="dark">SCHEDULE</Button>
+    </Link>
+    </div>
 
-    </CardGroup>    
+    <div className="col  mb-4 text-center">
+    <Link to={ `./instructors` }>
+    <Button className="card-title button-box" variant="dark">INSTRUCTORS</Button>
+    </Link>
+    </div>
+  </div>
+
+    {/*<div className="conteiner animate__animated animate__fadeIn" style={{marginLeft:"20px",marginRight:"20px"}}>
+
+      <div className="col-12 ">
+      
+        <span className="filt head-text">
+          <img className="imagen " alt="" src="./assets/home/1.jpg"/>
+          <div className='text-on-image'>
+             <h1 className='font-resize-cards text-on-image-style'> KIDS CLASS </h1>
+          </div>
+        </span>
+
+        <span className="filt head-text"><img className="imagen" alt="" src="./assets/home/2.jpg"/>
+          <div className='text-on-image'>
+             <h1 className='font-resize-cards text-on-image-style'> INTRO CLASS </h1>
+          </div>
+        </span>
+        
+        <span className="filt head-text"><img className="imagen" alt="" src="./assets/home/3.jpg"/>
+        <div className='text-on-image'>
+             <h1 className='font-resize-cards text-on-image-style'> SCHEDULE</h1>
+          </div>
+        </span>
+        
+        <span className="filt head-text"><img className="imagen" alt="" src="./assets/home/4.jpg"/>
+          <div className='text-on-image'>
+             <h1 className='font-resize-cards text-on-image-style'> INSTRUCTORS </h1>
+          </div>
+        </span>
+      </div>
+  </div>*/}
+    </>
   );
 };
